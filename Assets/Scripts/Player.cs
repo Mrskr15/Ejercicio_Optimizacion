a5f5a46ac75142125a5232d79b8fdb99a1c65e2e
Mrskr15 <oscar.gonga02@gmail.com>
2022-03-16 19:26:57 +0100

    Optimizacion

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,10 +9,21 @@ public class Player : MonoBehaviour
     public GameObject prefab;
     public int bulletType;
 
+    public float speed = 5f;
+
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetKey(KeyCode.D)) 
+        {
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.A)) 
+        {
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+
         if (Input.GetButtonDown("Jump"))
         {
             GameObject bullet = PoolManager.instance.GetPoolObject(bulletType);
