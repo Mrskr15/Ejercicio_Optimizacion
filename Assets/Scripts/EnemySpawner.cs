07330a25a1d3ff9ffc5df3b4531fce76391a04f5
Mrskr15 <oscar.gonga02@gmail.com>
2022-03-16 18:47:43 +0100

    Optimizacion

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -7,7 +7,7 @@ public class EnemySpawner : MonoBehaviour
     public Transform enemySpawner;
     public GameObject prefab;
     public int bulletType;
-    public float timeToSpawn = 3f, RangeSpawn = 3f;
+    public float timeToSpawn = 3f, RangeSpawn = 60f;
 
     void Start()
     {
@@ -17,9 +17,9 @@ public class EnemySpawner : MonoBehaviour
     // Update is called once per frame
     void Spawn()
     {
-        Vector3 SpawnPosition = new Vector3 (0,0,0);
+        Vector3 SpawnPosition = new Vector3 (0, 0, 0);
         SpawnPosition = this.transform.position + Random.onUnitSphere * RangeSpawn;
-        SpawnPosition = new Vector3 (0, 2, SpawnPosition.z);
+        SpawnPosition = new Vector3 (SpawnPosition.x, 0, 70);
 
         GameObject Enemy = PoolManager.instance.GetPoolObject(1);
         Enemy.transform.position = SpawnPosition;
